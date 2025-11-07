4cd91abf5011ce45c14a640a3781c4fbd0400c83
Md Towfik Omer <mdtowfikomer@gmail.com>
2025-11-07 12:23:47 +0530

    added home page route

--- BackendWithSQL/index.js
+++ BackendWithSQL/index.js
@@ -1,6 +1,14 @@
 require('dotenv').config();
+const express = require('express');
 const {faker} = require('@faker-js/faker');
 const mySql = require('mysql2');    // get the client
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 // Create the Connection to database
 const connection = mySql.createConnection({
@@ -24,23 +32,59 @@ let getRandomUser = () => {
 //quering the DB
 
 //inserts a new record into a database table named user using placeholder '?'.
-let query = "INSERT INTO user(id, username, email, password) VALUES ?"; // now it will take the whole array as 1 placeholder
-let userData = [];
-for(let i = 1; i<=1; i++){
-    userData.push(getRandomUser()); // 100 random user's data
-}
+// let query = "INSERT INTO user(id, username, email, password) VALUES ?"; // now it will take the whole array as 1 placeholder
+// let userData = [];
+// for(let i = 1; i<=1; i++){
+//     userData.push(getRandomUser()); // 100 random user's data
+// }
 
-try{
-    connection.query( query, [userData], (err, result)=>{ // parameter: query string, user's data, err for error message and result for 
-        if(err) throw err;
-        console.log(result); // result is an array
-        console.log(result.length);
-    });
-} catch(err){
-    console.log(err);
-}
+// try{
+//     connection.query( query, [userData], (err, result)=>{ // parameter: query string, user's data, callback function: err for error message and result for 
+//         if(err) throw err;
+//         console.log(result); // result is an array
+//         console.log(result.length);
+//     });
+// } catch(err){
+//     console.log(err);
+// }
 
-// closing the connection with DB
-connection.end();
+// // closing the connection with DB
+// connection.end();
 
 
+// Routing
+
+app.get('/', (req, res)=>{
+    // show no of user in DB
+    let q = `SELECT count(*) FROM user`;
+    try{
+        connection.query(q, (err, result)=>{
+            if(err) throw err;
+            let count = result[0]['count(*)'];
+            res.render("home.ejs", {count});
+        });
+    } catch(err){
+        res.send("Something went wrong");
+    }
+});
+
+app.get('/user',(req,res)=>{
+    // show users (email, id, username) via EJS
+});
+
+app.patch('/user/:id', (req, res)=>{
+    //username edit
+});
+
+app.post('/user', (req, res)=>{
+    // add new user
+});
+
+app.delete('/user/:id', (req, res)=>{
+    // delete user
+    // before deleting ask password from user..!
+});
+
+app.listen(port, (req, res)=>{
+    console.log(`Server is running at localhost: ${port}`);
+})
